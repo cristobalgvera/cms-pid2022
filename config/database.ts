85739797d5bb07504dd887c4fe94b6c31f1a3fd2
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2022-11-15 02:32:50 -0300

    feat(db): add postgres as database

--- config/database.ts
+++ config/database.ts
@@ -1,11 +1,20 @@
-import path from 'path';
+import fs from 'fs';
 
 export default ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "postgres",
     connection: {
-      filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", "strapi"),
+      password: env("DATABASE_PASSWORD", "strapi"),
+      schema: env("DATABASE_SCHEMA", "public"), // Not required
+      ssl: {
+        // From ./dist folder
+        ca: fs.readFileSync(`${__dirname}/../../ssl/ca-certificate-2021.crt`).toString(),
+      }
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
